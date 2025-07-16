ddb6c1924b511c61da3e59faeb8cca19b944a551
enderdev-v <endercrackgamer@outlook.com>
2025-07-15 18:42:23 -0600

    Bug css in mobile i fix it

--- src/assets/About.tsx
+++ src/assets/About.tsx
@@ -4,10 +4,10 @@ export default function About() {
         <section className="pt-4   mr-6 ml-8 my-12 " id="About">
             <h4 className="font-bold text-3xl dark:text-white-200 text-gray-800 text-center p-4 pb-12">About me!</h4>
             <div className="flex items-center object-center justify-center">
-                <div className="h-64 w-100  rounded-2xl shadow-lg dark:bg-gray-900/50 dark:shadow-gray-700 shadow-gray-900 bg-neutral-800/50 rotate-2 z-1">
-                    <div className="h-64 w-100 rounded-xl dark:bg-gray-800/50 bg-zinc-600/50 relative -rotate-3 dark:shadow-gray-600 shadow-gray-800">
-                        <div className="h-32 w-100 rotate-2 p-12">
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-200">
+                <div className="md:h-64 h-72 w-80 md:w-100  rounded-2xl shadow-lg dark:bg-gray-900/50 dark:shadow-gray-700 shadow-gray-900 bg-neutral-800/50 rotate-2 z-1">
+                    <div className="md:h-64 h-72 w-80 md:w-100 rounded-xl dark:bg-gray-800/50 bg-zinc-600/50 relative -rotate-3 dark:shadow-gray-600 shadow-gray-800">
+                        <div className="md:h-32 h-24 w-80 md:w-100 rotate-2 md:p-12 p-8">
+                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-200 sm:text-lg">
                                 <span className="font-bold">Just a simple dev</span>
                                 <br /><br />
                                 I develop somethings that I like, I like developing with React, NodeJS, and Typescript. 
