e4695259bbdf03a4959e50850c4299d4009994d0
enderdev-v <endercrackgamer@outlook.com>
2025-08-08 19:34:55 -0600

    feat: Add Projects without Create new component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,14 +1,14 @@
+import { ProjectButton } from "@/constants/projects";
 import Button from "@components/ui/Button";
 
 interface CardProps {
     img: string;
     title: string;
     description: string;
-    link: string;
-    href: string;
+    buttons: ProjectButton[]
 }
 
-export default function Card({ img, title, description, link, href }: CardProps) {
+export default function Card({ img, title, description, buttons }: CardProps) {
     return (
         <div className="h-80 w-64 mt-10 mb-10 dark:bg-gray-800 bg-gray-400 text-black-100 rounded-xl dark:text-white-0 mr-10">
             <div className="m-2">
@@ -17,12 +17,16 @@ export default function Card({ img, title, description, link, href }: CardProps)
                     <h3 className="text-lg font-bold mt-2">{title}</h3>
                     <p className="text-base">{description}</p>
                     <div className="mt-8">
-                        <Button variant="outline-info" href={href} target="_blank" rel="noopener noreferrer">
-                            <i className='bx bxl-html5' ></i> &nbsp; Project
-                        </Button>
-                        <Button variant="outline-info" href={link} target="_blank" rel="noopener noreferrer">
-                            <i className='bx bxl-github'></i> &nbsp; Repo
-                        </Button>
+                        {buttons.map((button) => {
+                            const { icon, label, link } = button;
+                            const href = link.startsWith("http") ? link : `https://${link}`;
+                            return (
+                                <Button variant="outline-info" href={href} target="_blank" rel="noopener noreferrer">
+                                    <i className={icon} ></i> &nbsp; {label}
+                                </Button>
+                            );
+                        })
+                    }
                     </div>
                 </div>
             </div>
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,18 +1,26 @@
+import { projects } from "@/constants/projects";
 import Navbar from "../components/Sections/navbar";
 import '../index.css'
 import Card from "@components/ProjectCard.tsx";
 
 export default function Projects() {
-    
-    const img = "https://camo.githubusercontent.com/69bc9cf59c4e3cdf1b820d36ebf29db25348af198c2cfab99ec8615873b49dea/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f62616e6e6572732f3836323930353231313030313530333737342f38353265663261316433356237353461323433396265656136663133353462612e776562703f73697a653d34303936"
+
     return (
-        
+
         <>
             <Navbar></Navbar>
             <div className="m-10 grid grid-cols-[repeat(auto-fit,minmax(220px,1fr))] gap-8">
-                <Card description="Just a simple bot for discord" href="https://github.com/enderdev-v/enderbot" img={img} link="https://github.com/enderdev-v/enderbot" title="enderbot"></Card>
-                <Card description="Is Close Code but util for discord" href="/" img={img} link="/" title="Ryosaki Bot"></Card>
-                <Card description="Just a simple design for the new tab fro browser :D" href="https://github.com/enderdev-v/DevStartPage" img={img} link="https://dev-start-page.vercel.app/" title="Dev new Tab"></Card>
+                {
+                    projects.map((project, index) => (
+                        <Card
+                            key={index}
+                            title={project.title}
+                            description={project.description}
+                            img={project.img}
+                            buttons={project.buttons}
+                        />
+                    ))
+                }
             </div>
         </>
     );
