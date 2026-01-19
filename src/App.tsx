ff36c58b0bcd35704c51d7ff137da4eff84e8f9c
enderdev-v <endercrackgamer@outlook.com>
2026-01-18 18:39:58 -0600

    something else

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Home from "./Pages/Home"
 import { Routes, Route } from "react-router";
-import "./css/index.css"
+import Home from "./Pages/Home";
 import Page404 from "./Pages/404";
 import Social from "./Pages/Social";
 import Projects from "./Pages/Projects";
+import "./css/index.css";
 
 export default function App() {
   return (
