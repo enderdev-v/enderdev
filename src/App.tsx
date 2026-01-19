ee8edb4f2d7d2f105eefe799ef592f8f614f51fa
enderdev-v <endercrackgamer@outlook.com>
2026-01-18 18:57:08 -0600

    something else

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./Pages/Home";
 import Page404 from "./Pages/404";
 import Social from "./Pages/Social";
 import Projects from "./Pages/Projects";
-import "./css/index.css";
+import "#css/index.css";
 
 export default function App() {
   return (
